556ec3014b93b0afeb8c89863abfdc463c1a8f00
pszewczykpl <piotr.szewczyk96@gmail.com>
2019-12-10 11:56:30 +0100

    QeezPy Package moved to src/qeezpy and created setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="QeezPy", # Replace with your own username
+    version="0.0.1",
+    author="Piotr Szewczyk",
+    author_email="piotr.szewczyk96@gmail.com",
+    description="Selenium WebDriver Python Framework to create POM tests",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pszewczykpl/QeezPy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
